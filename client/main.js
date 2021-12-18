3b4cae8f5a3073804add967749ab44c179607884
dtudury <david.tudury@gmail.com>
2021-12-17 23:28:42 -0800

    a basic ws server to play with

---
+++ client/main.js
@@ -0,0 +1,16 @@
+import { h, proxy, render } from './horseless.0.5.3.min.esm.js'
+import { PushDiff } from '../lib/pushdiff.js'
+
+const webSocket = new WebSocket('ws://localhost:3000')
+
+webSocket.onopen = event => {
+  console.log(event)
+  webSocket.send('hello there')
+}
+webSocket.onmessage = event => {
+  console.log(event)
+  console.log(event.data)
+}
+
+const pd = new PushDiff()
+console.log(pd)
---
+++ server.js
@@ -0,0 +1,26 @@
+import http from 'http'
+import handler from 'serve-handler'
+import { WebSocketServer } from 'ws'
+import { PushDiff } from './lib/pushdiff.js'
+
+const server = http.createServer((request, response) => {
+  return handler(request, response)
+})
+
+const wss = new WebSocketServer({ noServer: true })
+
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, ws => {
+    ws.on('message', data => {
+      console.log(`Received message ${data}`)
+    })
+  })
+})
+
+const port = 3000
+server.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`)
+})
+
+const pd = new PushDiff()
+console.log(pd)
