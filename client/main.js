4a6415a8d2d4bc03a274c5dde2934ae83ade0a02
dtudury <david.tudury@gmail.com>
2021-12-29 16:13:21 -0800

    let's chat

--- client/main.js
+++ client/main.js
@@ -14,6 +14,7 @@ webSocket.onmessage = event => {
 }
 
 const pd1 = (window.pd1 = new PushDiff())
+const pd11 = (window.pd11 = new PushDiff())
 console.log(pd1)
 
 const m = {
@@ -25,20 +26,35 @@ const m = {
   e: null
 }
 
+const i11 = pd1.valuesByIndex.length
+const i12 = pd1.toIndex(m, pd11)
+
 const pd2 = (window.pd2 = new PushDiff())
 
-const i11 = pd1.valuesByIndex.length
-const i12 = pd1.toIndex(m)
-
 pd2.toIndex([1, 2, 3])
+
+const patch = pd2.rebase(pd11)
+console.log(patch)
+const pd3 = (window.pd3 = pd11.applyPatch(patch))
+console.log(pd3)
+
+/*
 const changes = pd1.valuesByIndex.slice(i11, i12 + 1)
 console.log(i11, changes)
 const rebaseMap = pd2.rebase(i11, changes)
 console.log(rebaseMap)
 
 console.log('pd1', i12, pd1.fromIndex(i12))
-console.log('pd2', rebaseMap.get(i12), pd2.fromIndex(rebaseMap.get(i12)))
+const i2 = rebaseMap.get(i12)
+console.log('pd2', i2, pd2.fromIndex(i2))
 pd1.applyPatch(i11, changes, rebaseMap)
+console.log('pd1', i2, pd1.fromIndex(i2))
+
+const pd3 = (window.pd3 = new PushDiff())
+const pd31 = (window.pd31 = new PushDiff())
+pd3.toIndex(m, pd31)
+*/
+
 // console.log(rebaseMap.get(i12), pd2.fromIndex(rebaseMap.get(i12)))
 
 /*
@@ -60,3 +76,5 @@ pd.replace('/a/b/test', m, 'commit #: the second')
 console.log(JSON.stringify(pd.namespace, null, ' . '))
 console.log(pd.dereference('/a/b/test').value)
 */
+
+window.PushDiff = PushDiff
