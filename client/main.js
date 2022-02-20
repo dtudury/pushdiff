dc4cf0f2ce35a7ea5dceb12224ef139e9b21fe06
dtudury <david.tudury@gmail.com>
2022-02-20 08:49:13 -0800

    wip

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { h, proxy, render, watchFunction } from './horseless.0.5.4.min.esm.js'
 import { PushDiff } from '../lib/pushdiff.js'
 import { getByPath, setByPath } from '../lib/jsonPointer.js'
+import { StateAssistant } from '../lib/StateAssistant.js'
 
 const clone = o => JSON.parse(JSON.stringify(o))
 
@@ -9,38 +10,39 @@ const model = (window.model = proxy({
   local: null
 }))
 
-let pd = (window.pd = new PushDiff())
+let sa = (window.sa = new StateAssistant())
+window.pd = sa.pd
 
 const webSocket = new WebSocket('ws://localhost:3000')
 
 const diffs = {}
 
 webSocket.onopen = event => {
-  let index = 0
+  let id = 0
   watchFunction(() => {
     // debounce?
     const diff = new PushDiff()
-    diff.nextIndex = pd.nextIndex
-    const nsIndex = pd.toIndex(model.local, diff)
+    diff.nextIndex = sa.pd.nextIndex
+    const index = sa.pd.toIndex(model.local, diff)
     const valueEntries = Object.entries(diff.valuesByIndex)
-    diffs[index] = diff
+    diffs[id] = diff
     webSocket.send(
       JSON.stringify({
+        id,
         index,
-        nsIndex,
         values: Object.entries(diff.valuesByIndex)
       })
     )
-    ++index
+    ++id
   })
   webSocket.onmessage = event => {
     try {
-      const { index, nsIndex, mapping } = JSON.parse(event.data)
-      const diff = diffs[index]
+      const { id, index, mapping } = JSON.parse(event.data)
+      const diff = diffs[id]
       const translation = new Map(mapping)
       const translatedDiff = PushDiff.translateDiff(translation, diff)
-      pd = window.pd = PushDiff.mergeDiffs(pd, translatedDiff)
-      model.shared = pd.fromIndex(nsIndex)
+      sa.pd = window.pd = PushDiff.mergeDiffs(sa.pd, translatedDiff)
+      model.shared = sa.pd.fromIndex(index)
       console.log(JSON.parse(JSON.stringify(model.shared)))
     } catch (e) {
       console.error(event, event.data)
--- server.js
+++ server.js
@@ -1,44 +1,73 @@
+import { createWriteStream, readFileSync } from 'fs'
 import http from 'http'
 import handler from 'serve-handler'
 import { WebSocketServer } from 'ws'
 import { PushDiff } from './lib/pushdiff.js'
 
-const pd = new PushDiff()
-
-const server = http.createServer((request, response) => {
-  return handler(request, response)
-})
-
-const wss = new WebSocketServer({ noServer: true })
-
-server.on('upgrade', (request, socket, head) => {
-  wss.handleUpgrade(request, socket, head, ws => {
-    ws.on('message', data => {
-      try {
-        let { index, nsIndex, values } = JSON.parse(data)
-        const diff = new PushDiff()
-        diff.valuesByIndex = values.reduce((acc, [key, value]) => {
-          acc[key] = value
-          return acc
-        }, [])
-        const translation = PushDiff.applyDiff(pd, diff)
-        nsIndex = PushDiff.translate(nsIndex, translation)
-        console.log(pd.valuesByIndex)
-        console.log('translated nsIndex', nsIndex)
-        console.log(JSON.parse(JSON.stringify(pd.fromIndex(nsIndex))))
-        const mapping = [...translation]
-        ws.send(JSON.stringify({ index, nsIndex, mapping }))
-      } catch (error) {
-        console.error(error)
-      }
-    })
-    ws.on('close', event => {
-      console.log('close', event)
-    })
-  })
-})
-
 const port = 3000
 server.listen(port, () => {
   console.log(`Running at http://localhost:${port}`)
+
+  let pd = new PushDiff()
+
+  const logValueByIndex = offset => (value, index) =>
+    console.log(`${(offset + index).toString().padStart(9, ' ')}: ${value}`)
+
+  const historyPath = './temp.ndjson'
+  try {
+    const history = new PushDiff()
+    history.valuesByIndex = new Array(2).concat(
+      readFileSync(historyPath, 'utf8')
+        .split(/\n/)
+        .filter(Boolean)
+    )
+    pd = PushDiff.mergeDiffs(history)
+    history.valuesByIndex.forEach(logValueByIndex(0))
+    pd.nextIndex = history.valuesByIndex.length
+    console.log('pd.nextIndex', pd.nextIndex)
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
+  }
+  const history = createWriteStream(historyPath, { flags: 'a' })
+
+  const server = http.createServer((request, response) => {
+    return handler(request, response)
+  })
+
+  const wss = new WebSocketServer({ noServer: true })
+  const connections = new Set()
+
+  //keep alive?
+  server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, ws => {
+      connections.add(ws)
+      console.log(`connections: ${connections.size}`)
+      ws.on('message', data => {
+        try {
+          let { id, index, values } = JSON.parse(data)
+          const diff = new PushDiff()
+          diff.valuesByIndex = values.reduce((acc, [key, value]) => {
+            acc[key] = value
+            return acc
+          }, [])
+          const mark = pd.nextIndex
+          const translation = PushDiff.applyDiff(pd, diff)
+          const newValuesByIndex = pd.valuesByIndex.slice(mark)
+          newValuesByIndex.forEach(logValueByIndex(mark))
+          history.write(newValuesByIndex.map(value => `${value}\n`).join(''))
+          index = PushDiff.translate(index, translation)
+          const mapping = [...translation]
+          ws.send(JSON.stringify({ id, index, mapping }))
+          console.log('pd.nextIndex', pd.nextIndex)
+        } catch (error) {
+          console.error(error)
+        }
+      })
+      ws.on('close', event => {
+        console.log('close', event)
+        connections.delete(ws)
+        console.log(`connections: ${connections.size}`)
+      })
+    })
+  })
 })
